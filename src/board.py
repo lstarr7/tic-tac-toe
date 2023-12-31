2473e53be85ec0dcc9daad37bc71f11c50021332
Logan Starr <lstarr7@gatech.edu>
2023-12-31 13:56:47 -0500

    began board class

---
+++ src/board.py
@@ -0,0 +1,15 @@
+class Board:
+    def __init__(self):
+        #intialize the board
+        
+    def make_move(self, position, player):
+        # update the board with the players move
+
+    def is_win(self):
+        # check if someone won
+    
+    def is_tie(self):
+        #check if the game is a tie
+
+    def display(self):
+        #display board to consol for testing
