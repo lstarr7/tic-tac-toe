648e7be0b9e26d3f6d36194ccad65e3f03fb16b1
Logan Starr <lstarr7@gatech.edu>
2023-12-31 14:22:57 -0500

    board class methodoly done

--- src/board.py
+++ src/board.py
@@ -1,15 +1,44 @@
 class Board:
     def __init__(self):
-        #intialize the board
-        
+        # Create a 3x3 grid initialized with empty spaces
+        self.grid = [[" " for _ in range(3)] for _ in range(3)]
+
     def make_move(self, position, player):
         # update the board with the players move
-
-    def is_win(self):
-        # check if someone won
+        if self.grid[position[0]][position[1]] == " ":
+            self.grid[position[0]][position[1]] == player.symbol
+            return True
+        else:
+            return False
+    def is_win(self,symbol):
+        #check rows
+        for row in self.grid:
+            if all(cell == symbol for cell in row):
+                return True
+        
+        #check columns
+        for col in range(3):
+            if all(self.grid[row][col] == symbol for row in range(3)):
+                return True
+        
+        #check diagonals
+        if all(self.grid[i][i] == symbol for i in range(3)):
+            return True
+        if all(self.grid[i][2-i] == symbol for i in range(3)):
+            return True
+        
+        return False
+        
     
     def is_tie(self):
         #check if the game is a tie
+        for row in self.grid:
+            if any(cell == " " for cell in row):
+                return False # if cell is empty it is nto a tie
+        return True #if no cell is empty it is a tie
 
     def display(self):
-        #display board to consol for testing
+        for row in self.grid:
+           print(" | ".join(row))
+           if row != self.grid[-1]:
+               print("-" * 9)
---
+++ src/player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, symbol, name):
+        self.symbol = symbol
+        self.name = name
+    
+    def get_move(self):
+        # get the players move
